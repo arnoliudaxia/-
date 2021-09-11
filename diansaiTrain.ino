3868f03445f61143349b3edb9b43cf81f3f7ddde
Liuyifei <liu20161225@qq.com>
2021-09-11 09:20:58 +0800

    Abandon3Axis

--- diansaiTrain.ino
+++ diansaiTrain.ino
@@ -4,29 +4,13 @@
 //GamepadPSP gamepad;
 
 
-Acceleration acc;
-
 void setup() {
 
     Serial.begin(115200);
-    acc.init();
 }
 
 void loop()
 {
-    
-   // Serial.print("Start working\n");
-    acc.readAcc();
-    Serial.print(acc.x);
-    Serial.print(",");
-    Serial.print(acc.y);
-    Serial.print(",");
-    Serial.print(acc.z);
-    Serial.print(",");
-    Serial.print(acc.pitch);
-    Serial.print(",");
-    Serial.print(acc.roll);
-    Serial.print("\n");
-    delay(100);
+ 
 
 }
